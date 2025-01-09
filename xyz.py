d52a5fbea22cb106a31131071f49cc0808792f43
Bobby Sharma <141633552+bobbysharma05@users.noreply.github.com>
2025-01-09 10:25:49 +0530

    Update xyz.py

--- xyz.py
+++ xyz.py
@@ -7,18 +7,6 @@ original list: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 shifted by 4, to the left: [4, 5, 6, 7, 8, 9, 0, 1, 2, 3]
 shifted by 4, to the right: [6, 7, 8, 9, 0, 1, 2, 3, 4, 5]'''
 
-#part1 
-def shifted_list(k):
-    for i in range(0,len(mylist)):
-        shlist.append(mylist[i-k])
-    return "shlist"
-
-mylist = list(map(int, input("Enter the elements of list = ").split()))
-shlist = []
-print(mylist)
-print(Shifted_list(len(mylist)-1))
-
-#part2
 def shifted_list(k):
     if sh.lower() == "left":
         for i in range(0,len(mylist)):
@@ -28,11 +16,10 @@ def shifted_list(k):
         for i in range(0,len(mylist)):
             shlist.append(mylist[k-i])
         return "shlist"
-
-
+        
 mylist = list(map(int, input("Enter the elements of list = ").split()))
 j = int(input("Write the shifting number = "))
-SH = str(input("Shifting will be left or right = "))
+sh = str(input("Shifting will be left or right = "))
 shlist = []
 print(mylist)
 print(shifted_list(len(mylist)-1))

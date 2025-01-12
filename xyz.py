852428ab6acd1e1b89462f57d20aa2b211318669
Bobby Sharma <141633552+bobbysharma05@users.noreply.github.com>
2025-01-12 15:00:55 +0530

    Update xyz.py

--- xyz.py
+++ xyz.py
@@ -7,35 +7,33 @@ original list: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 shifted by 4, to the left: [4, 5, 6, 7, 8, 9, 0, 1, 2, 3]
 shifted by 4, to the right: [6, 7, 8, 9, 0, 1, 2, 3, 4, 5]'''
 
-#part1 
-def Shifted_list(k):
-    for i in range(0,len(mylist)):
-        shlist.append(mylist[i-k])
-    return shlist
+def shifted_list(k):
+    """
+    Shifts the list to the left or right by k positions.
 
-mylist = list(map(int, input("Enter the elements of list = ").split()))
-shlist = []
-print(mylist)
-print(Shifted_list(len(mylist)-1))
+    Args:
+    mylist (list): The input list of integers.
+    k (int): Number of positions to shift.
+    direction (str): Direction of the shift ('left' or 'right').
 
-#part2
-def Shifted_list(k):
-    if sh.lower() == "left":
+    Returns:
+    list: The shifted list.
+    """
+    if SH.lower() == "left":
         for i in range(0,len(mylist)):
             shlist.append(mylist[i-k])
-        return shlist
-    elif sh.lower()=="right":
-        for i in range(0,len(mylist)):
-            shlist.append(mylist[k-i])
-        return shlist
+        return "shlist"
 
+    for i in range(0,len(mylist)):
+        shlist.append(mylist[k-i])
+    return "shlist"
 
 mylist = list(map(int, input("Enter the elements of list = ").split()))
 j = int(input("Write the shifting number = "))
-sh = str(input("Shifting will be left or right = "))
+SH = str(input("Shifting will be left or right = "))
 shlist = []
 print(mylist)
-print(Shifted_list(len(mylist)-1))
+print(shifted_list(len(mylist)-1))
 
 #THis is my first question
 #New Comment
